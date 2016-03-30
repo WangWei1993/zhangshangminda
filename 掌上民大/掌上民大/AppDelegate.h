e87da377c9f5e9c2cd757209bb6c8f33b873cf00
王伟 <wangWei_Mac@wangweidemac-mini.local>
2016-03-30 23:57:38 +0800

    集成工作空间

---
+++ 掌上民大/掌上民大/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  掌上民大
+//
+//  Created by 王伟 on 16/3/30.
+//  Copyright © 2016年 王伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 掌上民大/掌上民大/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  掌上民大
+//
+//  Created by 王伟 on 16/3/30.
+//  Copyright © 2016年 王伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
